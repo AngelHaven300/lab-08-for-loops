3b3879bdb20a551c62905530f506126367ff0501
AngelHaven300 <angegabriel369@gmail.com>
2023-10-18 20:34:57 -0400

    largest

---
+++ largest.py
@@ -0,0 +1,12 @@
+highestnum = 0
+# initiate highest num as 0 and replace this 0 inside our for loop 
+for i in range(0, 4, 1):
+    userinput = input("number please...")
+    userint = int(userinput)
+    print("User entered: " + str(userinput))
+    if userint > highestnum:
+        highestnum = userint
+        print("Updating highest number...")
+    else:
+        print("This is not the highest number")
+    print("the highest number entered is: " + str(highestnum))
