a58363b4183be294c9efd14c07fb809cc3d99a76
AngelHaven300 <angegabriel369@gmail.com>
2023-10-19 12:04:25 -0400

    largest

---
+++ party.py
@@ -0,0 +1,11 @@
+ 
+print("lets party...")
+userinput = input("Lets party! How long till the party?")
+userint = int(userinput)
+if userint < 1:
+     print("Party Now !!!")
+else: 
+     for userint in range(userint, 0, 1): 
+        print(userint)
+if userint == 0:
+    print("Party Time !!!!!")        
